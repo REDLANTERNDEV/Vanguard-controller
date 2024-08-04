2503b5149fcd1cb31c241e7be5dd8f93997142b9
REDLANTERNDEV <87618221+REDLANTERNDEV@users.noreply.github.com>
2024-08-04 14:31:53 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Vanguard_Controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisableVanguard = new System.Windows.Forms.Button();
+            this.btnEnableVanguard = new System.Windows.Forms.Button();
+            this.btnRestartComputer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDisableVanguard
+            // 
+            this.btnDisableVanguard.BackColor = System.Drawing.Color.Brown;
+            this.btnDisableVanguard.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDisableVanguard.Location = new System.Drawing.Point(12, 12);
+            this.btnDisableVanguard.Name = "btnDisableVanguard";
+            this.btnDisableVanguard.Size = new System.Drawing.Size(200, 23);
+            this.btnDisableVanguard.TabIndex = 0;
+            this.btnDisableVanguard.Text = "Disable Vanguard";
+            this.btnDisableVanguard.UseVisualStyleBackColor = false;
+            this.btnDisableVanguard.Click += new System.EventHandler(this.btnDisableVanguard_Click);
+            // 
+            // btnEnableVanguard
+            // 
+            this.btnEnableVanguard.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnEnableVanguard.Location = new System.Drawing.Point(12, 41);
+            this.btnEnableVanguard.Name = "btnEnableVanguard";
+            this.btnEnableVanguard.Size = new System.Drawing.Size(200, 23);
+            this.btnEnableVanguard.TabIndex = 1;
+            this.btnEnableVanguard.Text = "Enable Vanguard";
+            this.btnEnableVanguard.UseVisualStyleBackColor = false;
+            this.btnEnableVanguard.Click += new System.EventHandler(this.btnEnableVanguard_Click);
+            // 
+            // btnRestartComputer
+            // 
+            this.btnRestartComputer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRestartComputer.Location = new System.Drawing.Point(12, 70);
+            this.btnRestartComputer.Name = "btnRestartComputer";
+            this.btnRestartComputer.Size = new System.Drawing.Size(200, 23);
+            this.btnRestartComputer.TabIndex = 2;
+            this.btnRestartComputer.Text = "Restart Computer";
+            this.btnRestartComputer.UseVisualStyleBackColor = false;
+            this.btnRestartComputer.Click += new System.EventHandler(this.btnRestartComputer_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(224, 105);
+            this.Controls.Add(this.btnRestartComputer);
+            this.Controls.Add(this.btnEnableVanguard);
+            this.Controls.Add(this.btnDisableVanguard);
+            this.Name = "Form1";
+            this.Text = "Vanguard Control";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisableVanguard;
+        private System.Windows.Forms.Button btnEnableVanguard;
+        private System.Windows.Forms.Button btnRestartComputer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vanguard_Controller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisableVanguard_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RunCommand("sc config vgc start= disabled");
+                RunCommand("sc config vgk start= disabled");
+                RunCommand("net stop vgc");
+                RunCommand("net stop vgk");
+                RunCommand("taskkill /IM vgtray.exe");
+
+                MessageBox.Show("Vanguard başarıyla kapatıldı.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
+            }
+        }
+
+        private void btnEnableVanguard_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RunCommand("sc config vgc start= demand");
+                RunCommand("sc config vgk start= system");
+
+                MessageBox.Show("Vanguard başarıyla etkinleştirildi.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
+            }
+        }
+
+        private void btnRestartComputer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RunCommand("shutdown /r /f /t 00");
+
+                MessageBox.Show("Bilgisayar yeniden başlatılıyor.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
+            }
+        }
+        private void RunCommand(string command)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
+            {
+                Verb = "runas", 
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(processInfo))
+            {
+                process.WaitForExit();
+                string output = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(output); 
+            }
+        }
+    }
+}
